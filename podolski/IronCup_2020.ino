df3725082ce8cf246a6475d91093201a24627a10
Eder Florencio <49597111+FlorencioEder@users.noreply.github.com>
2020-02-24 11:49:53 -0300

    Codigo utilizado em IronCup 2020
    Inicio das programações com funções  para evitar repetições de código

---
+++ podolski/IronCup_2020.ino
@@ -0,0 +1,255 @@
+bool direcao;/*
+  Define para qual direção o 
+  robo ira buscar o seu adversario 
+*/
+int estrategia = 0;/*
+  Define qual estrategia 
+  inicial o robo ira executar
+*/
+int oponente = 0;/*
+  Define qual posição 
+  esta o oponente
+*/
+
+void setup() {
+  //---Sensores de Linha---
+    pinMode(A0,INPUT); //Direito
+    pinMode(A1,INPUT); //Esquerdo
+  //-----------------------
+
+  //---Sensores de Distancia---
+    pinMode(A2,INPUT); //Direito
+    pinMode(A3,INPUT); //Frontal
+    pinMode(A4,INPUT); //Esquerdo
+  //-----------------------
+
+  //---Motores---
+    //Motor Esquerdo
+    pinMode(11,OUTPUT); //pwmB
+    pinMode(10,OUTPUT); //Bin
+    pinMode(9,OUTPUT);  //Bin
+
+    //Motor Direito
+    pinMode(7,OUTPUT);  //Ain
+    pinMode(6,OUTPUT);  //Ain
+    pinMode(5,OUTPUT);  //pwmA
+
+    //STBY
+    pinMode(8,OUTPUT);
+    digitalWrite(8,0); //Seta o STBY para desativado inicialmente
+  //-----------------------
+
+  //---DIP Switch:---
+    pinMode(12,INPUT);// Chave 4
+    //Direção de giro inicial da busca e/ou estrategia
+    /*
+      1 == Esquerda -- 0 == Direita
+      como todas as estrategias iniciais 
+      podem ser espelhadas utilizaremos 
+      uma variavel para dizer qual a 
+      direção que iremos seguir com esta estrategia
+      
+      as entradas do Switch abaixo combinadas 
+      podem render até oito estrategias 
+      iniciais diferentes de maneira binaria
+    */
+    pinMode(4,INPUT);// Chave 3
+    pinMode(3,INPUT);// Chave 2
+    pinMode(2,INPUT);// Chave 1
+  //-----------------------
+
+  pinMode(13,INPUT); //Botao de start (onofre)
+  while(digitalRead(13)==1){} //Aguarda o botao onofre ser ativado
+
+  delay(5000); //Aguarda os 5 segunndos
+  digitalWrite(8,1); //Seta o STBY para ativado
+  executa_estrategia();
+  //Começa o round efetivamente
+
+  //Serial.begin(9600);
+  //mantido para ocasional debug
+}
+
+void loop() {
+  while((analogRead(A1)>250) && (analogRead(A2)>250)){
+    //enquanto estiver no preto
+
+    busca_oponente();
+    switch (oponente) {
+      // Vide Tabela de Conversão no void busca_oponente()
+      case 0:
+        if (direcao) {
+          move('D','F', 150);
+          move('E','T', 150);
+        }else{
+          move('D','T', 150);
+          move('E','F', 150);
+        }
+          break;
+      case 1:
+        move('D','F', 50);
+        move('E','F', 250);
+        direcao = false;
+        break;
+      case 2:
+        move('D','F', 250);
+        move('E','F', 250);
+        break;
+      case 3:
+        move('D','F', 200);
+        move('E','F', 250);
+        direcao = false;
+        break;
+      case 4:
+        move('D','F', 50);
+        move('E','F', 250);
+        direcao = true;
+        break;
+      case 5:
+        move('D','F', 250);
+        move('E','F', 250);
+        break;
+      case 6:
+        move('D','F', 250);
+        move('E','F', 200);
+        direcao = true;
+        break;
+      case 7:
+        move('D','F', 250);
+        move('E','F', 250);
+        break;
+      default:
+      //caso a ariavel esteja com valor bugado apenas roda
+        if (direcao) {
+          move('D','F', 150);
+          move('E','T', 150);
+        }else{
+          move('D','T', 150);
+          move('E','F', 150);
+        }
+          break;
+    }
+  }
+  //se sair do while executa a rotina de linha
+  move('D','T', 200);
+  move('E','T', 200);
+  delay(400);
+}
+
+void move(char M, char S, int V) {
+  /*
+    M = qual motor 
+    'D' -> motor direito || 'E' -> motor esquerdo
+
+    S = quak sentido de rotação
+    'F' -> frente || 'T' -> tras
+
+    V = velocidade de rotação
+    0 ~ 255
+  */
+
+  if ((M == 'D') and (S == 'T')) {
+    digitalWrite(7,1);
+    digitalWrite(6,0);
+    analogWrite(5,V);
+  }else if ((M == 'D') and (S == 'F')) {
+    digitalWrite(7,0);
+    digitalWrite(6,1);
+    analogWrite(5,V);
+  }else if ((M == 'E') and (S == 'F')) {
+    digitalWrite(9,0);
+    digitalWrite(10,1);
+    analogWrite(11,V);
+  }else if ((M == 'E') and (S == 'T')) {
+    digitalWrite(9,1);
+    digitalWrite(10,0);
+    analogWrite(11,V);
+  }
+}
+
+void executa_estrategia(){
+  //Configurações iniciais
+    //Estipula a direção
+    if(digitalRead(12)==1){
+      //Seta para girar para Esquerda
+      direcao = true;
+    }else{
+      //Seta para girar para Direita
+      direcao = false;
+    }
+
+    //Estipula a estrategia inicial atraves de uma soma binaria
+    estrategia = 0;
+    if (digitalRead(4)==1){ estrategia += 2^0; }
+    if (digitalRead(3)==1){ estrategia += 2^1; }
+    if (digitalRead(2)==1){ estrategia += 2^2; }
+  //-----------------------
+
+
+
+
+  //
+  //
+  //
+  //
+  //ainda será preenchido com as estrategias iniciais 
+  //
+  //
+  //
+  //
+}
+
+void busca_oponente(){
+  /*
+    Checa quais sensores de distancia estão 
+    detectando o oponente e, apartir disso, estipula 
+    um valor para a variavel oponente
+
+    Da mesma maneira que é estipulada a estrategia
+    inicial, será utilizado um somatorio binario para 
+    combinar os resultaddos de leitura dos sensores de 
+    distancia em um int [vide tabela de conversão]
+  */
+
+  oponente = 0;//zera a variavel para nova interação
+
+  if (analogRead(A2)>20){ oponente += 2^0; }
+  if (analogRead(A3)>20){ oponente += 2^1; }
+  if (analogRead(A4)>20){ oponente += 2^2; }
+
+  /*
+
+  --------------TABELA DE CONVERSÃO----------------
+  ________________________________________________
+  |                     |                        |
+  |  valor da variavel  |  situação do oponente  |
+  |_____________________|________________________|
+  |                     |                        |
+  |          0          |     Fora de vista      |
+  |_____________________|________________________|
+  |                     |                        |
+  |          1          |  Totalmete a direita   |
+  |_____________________|________________________|
+  |                     |                        |
+  |          2          |    Longe e a frente    |
+  |_____________________|________________________|
+  |                     |                        |
+  |          3          |   Um pouco a direita   |
+  |_____________________|________________________|
+  |                     |                        |
+  |          4          |  Totalmete a esquerda  |
+  |_____________________|________________________|
+  |                     |                        |
+  |                     |  Impossivel, somente   |
+  |          5          |  laterais, sensor da   |
+  |                     |   frente quebrou???    |
+  |_____________________|________________________|
+  |                     |                        |
+  |          6          |   Um pouco a esquerda  |
+  |_____________________|________________________|
+  |                     |                        |
+  |          7          |   Totalmete a frente   |
+  |_____________________|________________________|
+
+  */
+}
