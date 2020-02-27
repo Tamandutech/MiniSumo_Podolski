a3566469e1040edb3c59877bdd2ac907706baa68
Eder Florencio <49597111+FlorencioEder@users.noreply.github.com>
2020-02-27 20:02:40 -0300

    Inclusão das primeiras estrategias
    Inclusão das primeiras estrategias e melhoria nos calculos binarios

--- podolski/IronCup_2020.ino
+++ podolski/IronCup_2020.ino
@@ -12,6 +12,10 @@ int oponente = 0;/*
 */
 
 void setup() {
+  Serial.begin(9600);
+  //Serial.print("oi");
+  //mantido para ocasional debug
+
   //---Sensores de Linha---
     pinMode(A0,INPUT); //Direito
     pinMode(A1,INPUT); //Esquerdo
@@ -54,8 +58,8 @@ void setup() {
       iniciais diferentes de maneira binaria
     */
     pinMode(4,INPUT);// Chave 3
-    pinMode(3,INPUT);// Chave 2
-    pinMode(2,INPUT);// Chave 1
+    pinMode(3,INPUT_PULLUP);// Chave 2
+    pinMode(2,INPUT_PULLUP);// Chave 1
   //-----------------------
 
   pinMode(13,INPUT); //Botao de start (onofre)
@@ -64,84 +68,85 @@ void setup() {
   digitalWrite(8,1); //Seta o STBY para ativado
   delay(2000); //Aguarda os 5 segunndos
   
-  //executa_estrategia();
+  executa_estrategia();
   //Começa o round efetivamente
 
-  Serial.begin(9600);
-  //mantido para ocasional debug
 }
 
 void loop() {
-  while((analogRead(A1)>500) && (analogRead(A0)>500)){
-    //enquanto estiver no preto
-  //while(true){
-    
-    /*Serial.print("\nLinhas--->Esquerda: ");
+  /*while(true){ 
+    //Debugsons-->descomente caso precise debubar e comente o outro
+    Serial.print("\nLinhas--->Esquerda: ");
     Serial.print(analogRead(A1));
     Serial.print("      Direita: ");
-    Serial.print(analogRead(A0));*/
+    Serial.print(analogRead(A0));
+    Serial.print("\nOponente---> ");
+    Serial.println(oponente);
+  }*/
+  
+  while((analogRead(A1)>400) && (analogRead(A0)>400)){
+    //enquanto estiver no preto
 
 
     busca_oponente();
-    //Serial.println(oponente);
     switch (oponente) {
       // Vide Tabela de Conversão no void busca_oponente()
       case 0:
         if (direcao) {
-          move('D','F', 75);
-          move('E','T', 75);
+          move('D','F', 150);
+          move('E','T', 150);
         }else{
-          move('D','T', 75);
-          move('E','F', 75);
+          move('D','T', 150);
+          move('E','F', 150);
         }
-          break;
+        break;
       case 1:
-        move('D','F', 25);
-        move('E','F', 100);
+        move('D','F', 50);
+        move('E','F', 255);
         direcao = false;
         break;
       case 2:
-        move('D','F', 100);
-       /move('E','F', 100)
+        move('D','F', 255);
+        move('E','F', 255);
         break;
       case 3:
-        move('D','F', 75);
-        move('E','F', 100);
+        move('D','F', 200);
+        move('E','F', 255);
         direcao = false;
         break;
       case 4:
-        move('D','F', 25);
-        move('E','F', 100);
+        move('D','F', 255);
+        move('E','F', 50);
         direcao = true;
         break;
       case 5:
-        move('D','F', 100);
-        move('E','F', 100)
+        move('D','F', 255);
+        move('E','F', 255);
         break;
       case 6:
-        move('D','F', 100);
-        move('E','F', 75);
+        move('D','F', 255);
+        move('E','F', 200);
         direcao = true;
         break;
       case 7:
-        move('D','F', 100);
-        move('E','F', 100);
+        move('D','F', 255);
+        move('E','F', 255);
         break;
       default:
       //caso a variavel esteja com valor bugado apenas roda
         if (direcao) {
-          move('D','F', 75);
-          move('E','T', 75);
+          move('D','F', 150);
+          move('E','T', 150);
         }else{
-          move('D','T', 75);
-          move('E','F', 75);
+          move('D','T', 150);
+          move('E','F', 150);
         }
-          break;
+        break;
     }
   }
   //se sair do while executa a rotina de linha
-  move('D','T', 200);
-  move('E','T', 200);
+  move('D','T', 255);
+  move('E','T', 255);
   delay(400);
 }
 
@@ -189,23 +194,174 @@ void executa_estrategia(){
 
     //Estipula a estrategia inicial atraves de uma soma binaria
     estrategia = 0;
-    if (digitalRead(4)==1){ estrategia += 2^0; }
-    if (digitalRead(3)==1){ estrategia += 2^1; }
-    if (digitalRead(2)==1){ estrategia += 2^2; }
+    if (digitalRead(4)==1){ estrategia += pow(2,0); }
+    if (digitalRead(3)==1){ estrategia += pow(2,1); }
+    if (digitalRead(2)==1){ estrategia += pow(2,2); }
+    
+    //Mais debug
+    Serial.print("chave3: ");
+    Serial.print(digitalRead(4));
+    Serial.print("      chave2: ");
+    Serial.print(digitalRead(3));
+    Serial.print("      chave1: ");
+    Serial.print(digitalRead(2));
+    Serial.print("      chave4: ");
+    Serial.print(digitalRead(12));
+    Serial.print("      estrategia: ");
+    Serial.print(estrategia);
+    
   //-----------------------
 
+  switch (estrategia){
+    case 0:
+      /*
+        -->Frentão:
+        Estrategia de ataque total,
+        somente para de avançar se 
+        percceber que o oponete esta 
+        escapando para algum dos lados
+      */
+      while(((oponente != 1) && (oponente != 4)) && ((analogRead(A1)>400) && (analogRead(A0)>400))) {
+        Serial.print("\nOponente---> ");
+        Serial.println(oponente);
+        switch (oponente){
+          case 3:
+            move('D','F', 200);
+            move('E','F', 255);
+            direcao = false;
+            break;
+          case 6:
+            move('D','F', 255);
+            move('E','F', 200);
+            direcao = true;
+            break;
+          default:
+            move('D','F', 255);
+            move('E','F', 255);
+            break;
+        }
+        busca_oponente();
+      }
+      break;
+    case 1:
+      /*
+        -->Babaca:
+        Estrategia de filho da put@.
+        (inpirada nas da Maua)
+        Posiconar o robo bem atras e reto, 
+        quando ele inicia o robo se dirige 
+        para um dos lado do dojo o mais 
+        rapido possivel e aguarda ate que 
+        o oponente passe em sua frente
+      */
 
+      //vira para o lado que ele vai aguardar
+      if (direcao){
+        move('D','F', 255);
+      }else{
+        move('E','F', 255);
+      }
+      delay(100);
 
+      //avança ate a beirada
+      move('D','F', 255);
+      move('E','F', 255);
+      delay(400);
 
-  //
-  //
-  //
-  //
-  //ainda será preenchido com as estrategias iniciais 
-  //
-  //
-  //
-  //
+      //vira de frente para o meio
+      if (direcao){
+        move('D','T', 255);
+        move('E','F', 255);
+      }else{
+        move('D','F', 255);
+        move('E','T', 255);
+      }
+      delay(200);
+      //para 
+      move('D','F', 0);
+      move('E','T', 0);
+
+      //aguarda perceber o oponente
+      busca_oponente();
+      while(oponente == 0){
+        busca_oponente();
+      }
+      break;
+    case 2:
+      /*
+        -->Babaca 2:
+        Estrategia de filho da put@ tambem.
+        (inpirada nas da Maua)
+        Posiconar o robo bem a frente e
+        torto uns 45°, quando ele inicia o 
+        robo se dirige para a borda onde 
+        esta apontando, vira e espera 
+        encontrar o oponente
+      */
+
+      //avança ate a beirada
+      move('D','F', 255);
+      move('E','F', 255);
+      delay(400);
+
+      //vira de frente para o meio
+      if (direcao){
+        move('D','T', 255);
+        move('E','F', 255);
+      }else{
+        move('D','F', 255);
+        move('E','T', 255);
+      }
+      delay(250);
+      //para 
+      move('D','F', 0);
+      move('E','T', 0);
+
+      //aguarda perceber o oponente
+      busca_oponente();
+      while(oponente == 0){
+        busca_oponente();
+      }
+      break;
+    case 3:
+      /*
+        -->Costas:
+        Caso de empate comessando
+        de costas para o oponente.
+        Vira 180° em velociade maxima.
+      */
+      if (direcao){
+        move('D','F', 255);
+        move('E','T', 255);
+      }else{
+        move('D','T', 255);
+        move('E','F', 255);
+      }
+      delay(200);
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 7:
+      /*
+        -->Barra:
+        Não faz nada, apenas inicia o loop de
+        busca. Bom p dar aquela enganada, ou não, 
+        as vezes acaba sendo o mais usado.
+      */
+      break;
+    default:
+      /*
+        -->Deu merda:
+        VAI QUE... se der alguma merda
+        c a variavel apenas pula para 
+        o loop geral tb.
+      */
+      break;
+  }
 }
 
 void busca_oponente(){
@@ -222,16 +378,17 @@ void busca_oponente(){
 
   oponente = 0;//zera a variavel para nova interação
 
-  if (analogRead(A2)>40){ oponente += 2^0; }//direita
-  if (analogRead(A3)>30){ oponente += 2^1; }//frontal
-  if (analogRead(A4)>40){ oponente += 2^2; }//esquerda
-  /*Serial.print("\nDistancia--->Esquerda: ");
+  if (analogRead(A2)>40){ oponente += pow(2,0); }//direita
+  if (analogRead(A3)>30){ oponente += pow(2,1); }//frontal
+  if (analogRead(A4)>40){ oponente += pow(2,2); }//esquerda
+  /*
+  //Debug novamente:
+  Serial.print("\nDistancia--->Esquerda: ");
   Serial.print(analogRead(A4));
   Serial.print("      Frente: ");
   Serial.print(analogRead(A3));
   Serial.print("      Direita: ");
   Serial.print(analogRead(A2));*/
-
   /*
 
   --------------TABELA DE CONVERSÃO----------------
